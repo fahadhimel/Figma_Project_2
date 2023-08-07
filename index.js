7a1a20a6fc8f14d6ea18d288df97cc6942db6c34
fahad himel <fahadhimel2@gmail.com>
2023-08-07 18:16:26 +0600

    Home

---
+++ index.js
@@ -0,0 +1,20 @@
+// setTimeout(()=>{
+//     console.log('settimeout')
+// },1000)
+
+// setInterval(()=> {
+//     console.log('setInterval')
+// }, 500)
+
+setTimeout(()=>{
+    console.log('settimeout')
+},0)
+
+
+console.log('out of timeout')
+console.log('out of timeout')
+console.log('out of timeout')
+console.log('out of timeout')
+console.log('out of timeout')
+console.log('out of timeout')
+console.log('out of timeout')
